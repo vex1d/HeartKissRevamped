459fc4f5d09f59d2a5f3e77b44f91a7a16481509
vex1d <AcidgorgonQQ@gmail.com>
2026-01-08 21:52:13 +0100

    devil hunter update

--- Games/DevilHunter.lua
+++ Games/DevilHunter.lua
@@ -41,87 +41,98 @@ local function EnsureSafePart()
 end
 EnsureSafePart()
 
+local function RunCleanupMission(ID, MissionType)
+     local args = {
+        "OverworldMissions",
+        {
+            Identification = ID,
+            Conditions = { "Bloodlust", "Flawless" },
+            Directive = "Cleanup Duty",
+            Request = "Engage"
+        }
+    }
+    RemoteFunction:InvokeServer(unpack(args))
+
+    local MissionAsset = CleanupDutyFolder:WaitForChild(MissionType, AUTOFARM_CONFIG.Timeout)
+    local TeleportPoint = MissionAsset and MissionAsset:WaitForChild("TeleportPoint", AUTOFARM_CONFIG.Timeout)
+    
+    if not TeleportPoint then 
+        warn("Failed to find Mission Assets/Teleport Point") 
+        return 
+    end
+
+    local root = PlayerUtils:GetRoot()
+    local arrived = false
+    
+    local startWait = tick()
+    repeat
+        if root and (root.Position - TeleportPoint.Position).Magnitude < 20 then
+            arrived = true
+        end
+        task.wait(0.1)
+    until arrived or (tick() - startWait > 8)
+
+    if not arrived then
+        warn("Game did not teleport character. Forcing teleport.")
+    end
+
+    local TomatoDevil = Workspace.World.Effects:WaitForChild("TomatoDevil", 15)
+    if not TomatoDevil then warn("TomatoDevil not found") return end
+    
+    print("Found Target. Engaging.")
+    
+    PlayerUtils:Float(true)
+    PlayerUtils:SetNoclip(true)
+    
+    root.CFrame = TeleportPoint.CFrame * CFrame.new(0, AUTOFARM_CONFIG.TeleportOffset, 0)
+    task.wait(0.2)
+    root.CFrame = TomatoDevil:GetPivot() * CFrame.new(0, AUTOFARM_CONFIG.TeleportOffset, 0)
+    
+    local TPrompt = TomatoDevil:WaitForChild("RootPart"):WaitForChild("Prompt", 5)
+    PlayerUtils:SpamPrompt(TPrompt, AUTOFARM_CONFIG.InteractAttempts)
+
+    root.CFrame = AUTOFARM_CONFIG.SafeZonePos * CFrame.new(0, 5, 0)
+    task.wait(1.5)
+
+    local TurnInPoint = MissionAsset:FindFirstChild("TurnIn")
+    
+    if TurnInPoint then
+        while PlayerUtils:CheckForClosePlayers(TurnInPoint, AUTOFARM_CONFIG.SafeDistance) do
+            warn("Player nearby, waiting...")
+            task.wait(1)
+        end
+
+        local TurnInPrompt = TurnInPoint:FindFirstChild("Prompt")
+        if TurnInPrompt then
+            root.CFrame = TurnInPoint.CFrame * CFrame.new(0, -20, 0)
+            PlayerUtils:SpamPrompt(TurnInPrompt, 50)
+        end
+    end
+
+    PlayerUtils:Float(false)
+    PlayerUtils:SetNoclip(false)
+    local finalRoot = PlayerUtils:GetRoot()
+    if finalRoot then
+        finalRoot.CanCollide = false
+        finalRoot.CFrame = AUTOFARM_CONFIG.EndZonePos
+    end
+    print("Mission Cycle Complete")
+end
+
 local function ToggleAutoFarm(MissionType: string, Enabled: boolean)
     if MissionType == "Cleanup Duty" then
         while Enabled do
-            warn("Starting Mission:", ID)
-            
-            local args = {
-                "OverworldMissions",
-                {
-                    Identification = ID,
-                    Conditions = { "Bloodlust", "Flawless" },
-                    Directive = "Cleanup Duty",
-                    Request = "Engage"
-                }
-            }
-            RemoteFunction:InvokeServer(unpack(args))
+            local availableMissions = RemoteFunction:InvokeServer("RequestLocationData", {"Cleanup Duty"})
 
-            local MissionAsset = CleanupDutyFolder:WaitForChild(MissionType, AUTOFARM_CONFIG.Timeout)
-            local TeleportPoint = MissionAsset and MissionAsset:WaitForChild("TeleportPoint", AUTOFARM_CONFIG.Timeout)
-            
-            if not TeleportPoint then 
-                warn("Failed to find Mission Assets/Teleport Point") 
-                return 
-            end
-
-            local root = PlayerUtils:GetRoot()
-            local arrived = false
-            
-            local startWait = tick()
-            repeat
-                if root and (root.Position - TeleportPoint.Position).Magnitude < 20 then
-                    arrived = true
+            if availableMissions then
+                for Id, MissionType in availableMissions do
+                    RunCleanupMission(Id, MissionType)
+                    break
                 end
-                task.wait(0.1)
-            until arrived or (tick() - startWait > 8)
-
-            if not arrived then
-                warn("Game did not teleport character. Forcing teleport.")
+            else
+                warn("No missions available")
             end
-
-            local TomatoDevil = Workspace.World.Effects:WaitForChild("TomatoDevil", 15)
-            if not TomatoDevil then warn("TomatoDevil not found") return end
-            
-            print("Found Target. Engaging.")
-            
-            PlayerUtils:Float(true)
-            PlayerUtils:SetNoclip(true)
-            
-            root.CFrame = TeleportPoint.CFrame * CFrame.new(0, AUTOFARM_CONFIG.TeleportOffset, 0)
-            task.wait(0.2)
-            root.CFrame = TomatoDevil:GetPivot() * CFrame.new(0, AUTOFARM_CONFIG.TeleportOffset, 0)
-            
-            local TPrompt = TomatoDevil:WaitForChild("RootPart"):WaitForChild("Prompt", 5)
-            PlayerUtils:SpamPrompt(TPrompt, AUTOFARM_CONFIG.InteractAttempts)
-
-            root.CFrame = AUTOFARM_CONFIG.SafeZonePos * CFrame.new(0, 5, 0)
-            task.wait(1.5)
-
-            local TurnInPoint = MissionAsset:FindFirstChild("TurnIn")
-            
-            if TurnInPoint then
-                while PlayerUtils:CheckForClosePlayers(TurnInPoint, AUTOFARM_CONFIG.SafeDistance) do
-                    warn("Player nearby, waiting...")
-                    task.wait(1)
-                end
-
-                local TurnInPrompt = TurnInPoint:FindFirstChild("Prompt")
-                if TurnInPrompt then
-                    root.CFrame = TurnInPoint.CFrame * CFrame.new(0, -20, 0)
-                    PlayerUtils:SpamPrompt(TurnInPrompt, 50)
-                end
-            end
-
-            PlayerUtils:Float(false)
-            PlayerUtils:SetNoclip(false)
-            local finalRoot = PlayerUtils:GetRoot()
-            if finalRoot then
-                finalRoot.CanCollide = false
-                finalRoot.CFrame = AUTOFARM_CONFIG.EndZonePos
-            end
-            print("Mission Cycle Complete")
-
+           
             task.wait(AUTOFARM_CONFIG.Timeout)
             warn("Starting Next Mission...")
         end
