2d7eb68b5674f2e417b1e0f598b1750800aa4804
vex1d <AcidgorgonQQ@gmail.com>
2026-01-08 21:13:49 +0100

    s

--- Games/DevilHunter.lua
+++ Games/DevilHunter.lua
@@ -1,9 +1,9 @@
 local Players = game:GetService("Players")
-local Lib = require("Utils/Library")
+local Lib = require("GUI/Library")
 local maid = require("Utils/Maid")
-local ParryTimings = require("Configs/ParryList")
+local ParryTimings = require("GameUtils/Timings/DevilHunterParryTimings")
 
-local window = Lib.new("Heartkiss", UDim2.fromScale(488, 518), Enum.KeyCode.RightControl)
+local window = Lib.new("HeartKiss", UDim2.fromScale(488, 518), Enum.KeyCode.RightControl)
 
 local MainTab = window:Tab("Main")
 local AutoFarm = window:Tab("AutoFarm")
