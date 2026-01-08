3b7b4a8014a279f5a38ab11dddf68c68e337b9c0
vex1d <AcidgorgonQQ@gmail.com>
2026-01-08 20:17:22 +0100

    Changes2

--- Main.lua
+++ Main.lua
@@ -1,3 +1,8 @@
 local Services = require("Utils/Services")
+local Lib = require("Library/UILib")
 
-print("HI from Main.lua")
+local window = Lib.new("HeartKiss Revamped", UDim2.fromScale(600, 400), Enum.KeyCode.RightControl)
+
+local mainTab = window:Tab("Main")
+local settingsTab = window:Tab("Settings")
+
